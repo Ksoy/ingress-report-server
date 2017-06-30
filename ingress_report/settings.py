8c32cd089c9e98822cd9f4a82c4dcab03bb26c95
Ksoy <ksoyam95@gmail.com>
2017-07-01 01:26:57 +0800

    close debug

--- ingress_report/settings.py
+++ ingress_report/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vgh^%@srm_j119!x5s0sx(4*js9$w9f@ww1t@$ik1)cb0kg(v='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ "*"]
 
