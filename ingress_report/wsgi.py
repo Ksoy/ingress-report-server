33bf4ebb49f91a7cc528cc4dbca982b1fc1e62aa
ksoy <ksoyam95@gmail.com>
2017-07-26 12:38:49 +0800

    update wsgi

--- ingress_report/wsgi.py
+++ ingress_report/wsgi.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 WSGI config for ingress_report project.
 
@@ -8,9 +9,14 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import site
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ingress_report.settings")
+site.addsitedir('/home/fuckagents/ingress-report-server/venv/lib/python3.5/site-packages')
+sys.path.append('/home/fuckagents/ingress-report-server')
 
 application = get_wsgi_application()
+
